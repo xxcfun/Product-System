49f0b9ff124b37f257de976bbf7ade2bc5e98a71
hhdMrLion <55070348+hhdMrLion@users.noreply.github.com>
2020-11-11 17:33:38 +0800

    'add-produce-admin'

--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from product.models import Product
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('order', 'user', 'owen_num', 'status', 'created_time', 'updated_time')
+    list_per_page = 10
+    ordering = ['-created_time']
